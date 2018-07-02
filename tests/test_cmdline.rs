3f018dca6d5d92caabeb98d99a666ec1b318a598
Ben J Woodcroft <donttrustben@gmail.com>
2018-07-02 11:09:42 +1000

    tests: Use tempfile.

--- tests/test_cmdline.rs
+++ tests/test_cmdline.rs
@@ -4,22 +4,27 @@ extern crate assert_cli;
 #[cfg(test)]
 mod tests {
     use assert_cli::Assert;
+    extern crate tempfile;
 
     #[test]
     fn test_filter_all_reads(){
+        let tf: tempfile::NamedTempFile = tempfile::NamedTempFile::new().unwrap();
+        let t = tf.path().to_str().unwrap();
         Assert::main_binary()
             .with_args(&[
                 "filter",
                 "-b",
                 "tests/data/2seqs.bad_read.1.bam",
                 "-o",
-                "/tmp/o"]).succeeds().unwrap();
-        assert_cmd!(samtools view "/tmp/o").stdout().contains("1\t99\tseq1")
-            .unwrap();
+                t]).succeeds().unwrap();
+        Assert::command(&["samtools","view",t])
+            .stdout().contains("1\t99\tseq1").unwrap();
     }
 
     #[test]
     fn test_filter_filter_out(){
+        let tf: tempfile::NamedTempFile = tempfile::NamedTempFile::new().unwrap();
+        let t = tf.path().to_str().unwrap();
         Assert::main_binary()
             .with_args(&[
                 "filter",
@@ -29,7 +34,7 @@ mod tests {
                 "tests/data/2seqs.bad_read.1.bam",
                 "-o",
                 "/tmp/o"]).succeeds().unwrap();
-        assert_cmd!(samtools view "/tmp/o").stdout().doesnt_contain("1\t99\tseq1")
-            .unwrap();
+        Assert::command(&["samtools","view",t])
+            .stdout().doesnt_contain("1\t99\tseq1").unwrap();
     }
 }
