22fec6466ac80f68e9ced739c400790af8bd6231
Ben Woodcroft <donttrustben@gmail.com>
2024-03-18 15:05:31 +1000

    external_command_checker: Use crate which.
    Suggested by: @nickp60
    
    #207

--- src/external_command_checker.rs
+++ src/external_command_checker.rs
@@ -1,32 +1,31 @@
 use bird_tool_utils::external_command_checker::*;
 
 pub fn check_for_bwa() {
-    check_for_external_command_presence("BWA", "which bwa").expect("Failed to find installed BWA");
+    check_for_external_command_presence_with_which("bwa").expect("Failed to find installed BWA");
 }
 
 pub fn check_for_bwa_mem2() {
-    check_for_external_command_presence("BWA", "which bwa-mem2")
-        .expect("Failed to find installed BWA");
+    check_for_external_command_presence_with_which("bwa").expect("Failed to find installed BWA");
     default_version_check("bwa-mem2", "2.0", false, Some("bwa-mem2 version"))
         .expect("Failed to find sufficient version of bwa-mem2");
 }
 
 pub fn check_for_minimap2() {
-    check_for_external_command_presence("minimap2", "which minimap2")
+    check_for_external_command_presence_with_which("minimap2")
         .expect("Failed to find installed minimap2");
     default_version_check("minimap2", "2.24-r1122", false, None)
         .expect("Failed to find sufficient version of minimap2");
 }
 
 pub fn check_for_samtools() {
-    check_for_external_command_presence("samtools", "which samtools")
+    check_for_external_command_presence_with_which("samtools")
         .expect("Failed to find installed samtools");
     default_version_check("samtools", "1.9", false, None)
         .expect("Failed to find sufficient version of samtools");
 }
 
 pub fn check_for_strobealign() {
-    check_for_external_command_presence("strobealign", "which strobealign")
+    check_for_external_command_presence_with_which("strobealign")
         .expect("Failed to find installed strobealign");
     default_version_check("strobealign", "0.11.0", false, None)
         .expect("Failed to find sufficient version of strobealign");
